3021be596c2c89f451a7189f60bd4472377b611c
cris <crismary04@gmail.com>
2017-05-01 02:49:22 -0400

    hice las migraciones, los modelos de Post e ImgOfPost, tambien acomode el setting

--- BlogComputacion/blog/models.py
+++ BlogComputacion/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    text = models.TextField()
+    # falta autor
+    date = models.DateField()
+    cont_vist = models.IntegerField()
+
+class ImgOfPost(models.Model):
+    #img = models.ImageField()
+    Post = models.ForeignKey(Post, on_delete = models.CASCADE)
