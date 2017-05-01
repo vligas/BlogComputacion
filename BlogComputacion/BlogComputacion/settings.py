ebbadf2bc6223e81bbbcfa9d76b4a5a1d5c7afeb
vligas <vligin2@gmail.com>
2017-05-01 03:14:02 -0400

    Cree 3 rutas basicas, templates, y config statics

--- BlogComputacion/BlogComputacion/settings.py
+++ BlogComputacion/BlogComputacion/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -116,5 +117,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
--- BlogComputacion/blog/views.py
+++ BlogComputacion/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'blog/pages/index.html')
+
+def showAll(request):
+    return HttpResponse('Aqui estan todos los Posts')
+
+def showOne(request, id):
+    return HttpResponse('se ingreso el id ' + id)
