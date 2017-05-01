f31342740bb6661450be86eefcf6b9eaf711370b
vligas <vligin2@gmail.com>
2017-05-01 19:40:25 -0400

    acomode error de sintaxis en post.admin, cree super usuario

--- BlogComputacion/blog/admin.py
+++ BlogComputacion/blog/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-admin.site.Register(Post) # registe los Posts para que se vean en el admin panel
+admin.site.register(Post) # registe los Posts para que se vean en el admin panel
