6cc5dd0f67b1a99ed362a290f5e467e060a34136
cris <crismary04@gmail.com>
2017-05-02 16:05:58 -0400

    Agrege el archivo urls a la app Users yacomode las lineas correspondientes en setting y urls globales

--- BlogComputacion/BlogComputacion/urls.py
+++ BlogComputacion/BlogComputacion/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('blog.urls'))    # Incluye las urls de BLOG cuando se solicita la direccion base
+    url(r'^', include('blog.urls')),    # Incluye las urls de BLOG cuando se solicita la direccion base
+    url(r'^users/', include('users.urls'))
 ]
