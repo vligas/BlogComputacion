57671214809b213510bf81e9a30e465d9d0a1c10
cris <crismary04@gmail.com>
2017-05-01 21:18:29 -0400

    El cambio para que el css se viera

--- BlogComputacion/BlogComputacion/settings.py
+++ BlogComputacion/BlogComputacion/settings.py
@@ -118,3 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
+
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
