32c56d90657ace2844569b7905f44b1632b8f621
cris <crismary04@gmail.com>
2017-05-02 17:59:42 -0400

    Le agregue el forms.py asi que quedo listo, solo falta agregarle estilos

--- BlogComputacion/users/views.py
+++ BlogComputacion/users/views.py
@@ -2,9 +2,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 #from django.core.urlresolvers import reverser_lazy
+from .forms import RegisterForm
 
 class UserRegister(CreateView):
     model = User
     template_name = "users/register.html"
-    form_class = UserCreationForm
+    form_class = RegisterForm
     #success_url = reverser_lazy() Esto es para la pag a la que se dirige luego de registrar
