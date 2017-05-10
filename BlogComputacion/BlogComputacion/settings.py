6a57b3551d51d696155b068c27f817d885aeee92
vligas <vligin2@gmail.com>
2017-05-10 09:16:38 -0400

    Agregue la direccion del a carpeta MEDIA y la agregue al .gitignore

--- BlogComputacion/BlogComputacion/settings.py
+++ BlogComputacion/BlogComputacion/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
 
