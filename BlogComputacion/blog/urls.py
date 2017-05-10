adbd1e1c8e73a3f91e1d25fd31e9645e9b4ec13c
vligas <vligin2@gmail.com>
2017-05-10 07:52:14 -0400

    Agregue Url para crear Post

--- BlogComputacion/blog/urls.py
+++ BlogComputacion/blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),  # Index de la pagina
     url(r'^posts/$', views.showAll, name='showAll'),  # Muestra todos los posts
     url(r'^contact/$', views.contact, name='contact'),  # Muestra todos los posts
-    url(r'^posts/(?P<id>[0-9]+)$', views.showOne, name='showOne'),  # Muestra un post en especifico con todos sus detalles
+    url(r'^posts/(?P<id>[0-9]+)/$', views.showOne, name='showOne'),  # Muestra un post en especifico con todos sus detalles
+    url(r'^create_post/$', views.createPost, name="create"), # en esta url cuando se realiza una peticion GET se muestra el formulario, y cuando es una peticion POST se procesa y se redirecciona en caso de ser correcto
     url(r'^enviarSugerencia/', views.enviarSugerencia, name = 'enviarSugerencia')
 ]
