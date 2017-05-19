2c016937b18268f6c5ef985294255b02f3e55043
cris <crismary04@gmail.com>
2017-05-19 15:35:07 -0400

    Hasta aqui se agrego la classe categoria y se puede ver desde el admin

--- BlogComputacion/blog/admin.py
+++ BlogComputacion/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, Category
 # Register your models here.
 
 admin.site.register(Post) # registe los Posts para que se vean en el admin panel
 admin.site.register(Comment)
+admin.site.register(Category)
--- BlogComputacion/blog/models.py
+++ BlogComputacion/blog/models.py
@@ -5,6 +5,9 @@ from django.core.urlresolvers import reverse_lazy
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length = 100)
+
 class Post(models.Model):
     title = models.CharField(max_length = 100)
     text = models.TextField()
@@ -13,6 +16,7 @@ class Post(models.Model):
     created_at = models.DateField(auto_now_add=True) # para que se llene automaticamente al instanciar el objeto
     cont_vist = models.IntegerField(default=0)
     cover = models.ImageField()
+    category = models.ManyToManyField(Category)
 
     def get_absolute_url(self):
         return reverse("showOne", kwargs={"id":self.id})   # blog:showOne es invalido ya que no tenemos un "namespace" llamado blog
